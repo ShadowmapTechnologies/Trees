41e99d569afa81488d3479154c49ee77bf15f6be
Simon Mulser <simonmulser@gmail.com>
2022-07-13 16:11:35 +0200

    Fix transformations

--- sources.js
+++ sources.js
@@ -19,11 +19,10 @@ export const sources = [
     filename: "BAUMKATOGD.csv",
     url: "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:BAUMKATOGD&srsName=EPSG:4326&outputFormat=csv",
     fieldTransformations: {
-      circumference: (tree) =>
-        Math.round(tree.properties.STAMMUMFANG * 10) / 1000,
+      circumference: (tree) => Math.round(tree.STAMMUMFANG * 10) / 1000,
       diameter_crown: "crown_diameter",
-      height: (tree) => tree.properties.BAUMHOEHE * 5 - 2.5,
-      diameter_crown: (tree) => tree.properties.KRONENDURCHMESSER * 3 - 1.5,
+      height: (tree) => tree.BAUMHOEHE * 5 - 2.5,
+      diameter_crown: (tree) => tree.KRONENDURCHMESSER * 3 - 1.5,
     },
     area: [
       [8922, 5672],
